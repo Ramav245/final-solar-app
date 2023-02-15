99b64449da717b21ec304073333694918c4966f4
Rama Venkataraman <46733091+Ramav245@users.noreply.github.com>
2023-02-14 20:54:06 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,112 +1,27 @@
+import React from 'react';
+import './App.css';
+import { HashRouter as Router, Routes, Route}
+	from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import WhySolar from './routes/WhySolar';
+import GoSolar from './routes/GoSolar';
 
+function App() {
+  return (
+    <>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/whySolar' element={<WhySolar/>} />
+      </Routes>
+    </>
 
-.hero 
-{
-	height: 100vh;
-	background: linear-gradient(-45deg, rgba(229,93,135,.7), rgba(95,195,228,.7)), url(https://picsum.photos/g/2000/1200?image=443) center center / cover no-repeat;
+  );
 }
 
-.hero .buttonHero
-{
-	padding-left: 5%;
-	padding-top: 30%;
-}
-
-.aboutUs .buttonAboutUs
-{
-	text-align: center;
-}
-
-p
-{
-padding-left: 10%;
-padding-right: 10%;
-text-align: center;
-}
-
-
-.hero h1
-{
-	position: absolute;
-	top: 35%;
-	margin-left: 5%;
-	color: #fff;
-	font-size: 75px;
-	text-align: center;
-	white-space: nowrap;
-}
-
-.hero h2
-{
-	font-size: 25px;
-	margin-left: 5%; 
-	position: absolute;
-	top: 45%;
-	color: #fff;
-
-	white-space: inherit;
-}
-
-
-h3
-{
-	text-align: center;
-	padding: 5rem;
-}
-
-
-
-.bootstrapCard
-{
-	padding-inline: 25px;
-	padding-top: 100px;
-}
-
-/* unvisited link */
-a:link {
-	color: white;
-	text-decoration: none;
-  }
-  
-  /* visited link */
-  a:visited {
-	color: white;
-	text-decoration: none;
-  }
-  
-  /* mouse over link */
-  a:hover {
-	color: hotpink;
-	text-decoration: none;
-  }
-  
-  /* selected link */
-  a:active {
-	color: white;
-	text-decoration: none;
-  }
-
-  .footer {
-	position: relative;
-	margin-top: 10rem;
-	padding: 5rem;
-	width: 100%;
-	height: 100%;
-	background-color: green;
-	color: white;
-	text-align: center;
- }
-
- img
- {
-	padding: 1rem;
-	
- }
-
- .navbar-brand {
-    position: relative;
-    background: url(./images/title.png);
-    width: 175px;
-	height: 50px;
-    background-size:cover;
-}
+export default App;
