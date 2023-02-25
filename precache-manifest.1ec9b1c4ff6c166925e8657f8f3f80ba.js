b92ed89912d9f2a488cc5946946ea7cdfa6d6cb7
Ramav245 <ramav245@gmail.com>
2023-02-24 20:41:29 -0800

    Updates

---
+++ precache-manifest.1ec9b1c4ff6c166925e8657f8f3f80ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "23a8011e21fd6a2e2bcf",
+    "url": "/final-solar-app/static/css/main.e614c9cf.chunk.css"
+  },
+  {
+    "revision": "23a8011e21fd6a2e2bcf",
+    "url": "/final-solar-app/static/js/main.f23ef10e.chunk.js"
+  },
+  {
+    "revision": "8c281a3d36d0680bb5e6",
+    "url": "/final-solar-app/static/js/runtime~main.6e9939e5.js"
+  },
+  {
+    "revision": "c5a7deb568c148f5827d",
+    "url": "/final-solar-app/static/css/2.85930dc7.chunk.css"
+  },
+  {
+    "revision": "c5a7deb568c148f5827d",
+    "url": "/final-solar-app/static/js/2.dd7a6474.chunk.js"
+  },
+  {
+    "revision": "6da42209a5eb5113a092",
+    "url": "/final-solar-app/static/js/3.5e6a9889.chunk.js"
+  },
+  {
+    "revision": "b5dff80bdd832d37de2ecec640091c23",
+    "url": "/final-solar-app/static/media/header.b5dff80b.png"
+  },
+  {
+    "revision": "5f50be68204d17b7a3ce985722bad40c",
+    "url": "/final-solar-app/index.html"
+  }
+];
